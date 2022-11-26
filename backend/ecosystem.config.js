96ceda49ef9d21e430ac067461e39b383b42ef4c
Красильникова Оксана Сергеевна <O.Krasilnikova@ase-ec.ru>
2022-11-26 21:57:12 +0300

    pm2 deploy

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build',
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,31 @@
+const dotenev = require('dotenv');
+
+dotenev.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name   : "app-frontend",
+      script : ".src/index.js"
+    },
+  ],
+
+  deploy : {
+    production : {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy' : 'cd frontend && npm install && npm run build',
+    }
+  }
+};
